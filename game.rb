21c115a7b3b5fecc9d6bea1e6e9ed76659a484e7
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-16 19:07:34 +0300

    Adding start method to game

--- game.rb
+++ game.rb
@@ -4,5 +4,23 @@ class Game
 
   def initialize(ui)
     @ui = ui
+    @players = []
+  end
+
+  def start
+    name = ui.request('Введите имя пользователя')
+    user = UserPlayer.new(name)
+    dealer = ComputerPlayer.new(DEALER_NAME)
+    players << user << dealer
+    play_round
+  end
+
+  private
+  attr_accessor :players, :cards_opened, :game_over
+
+  def play_round
+    self.cards_opened = false
+    self.game_over = false
+    
   end
 end

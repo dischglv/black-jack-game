8ddfeb4a9eac97ea3e2cf9d600cdfe7949740a35
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-16 09:43:15 +0300

    Adding all_suits and all_ranks methods

--- card.rb
+++ card.rb
@@ -8,4 +8,21 @@ class Card
     @suit = suit
     @rank = rank
   end
+
+  def string_representation
+  end
+
+  private
+  def all_suits
+    { 
+      Spades: "\u2660",
+      Hearts: "\u2665",
+      Diamonds: "\u2666",
+      Clubs: "\u2663"
+    }
+  end
+
+  def all_ranks
+    ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+  end
 end

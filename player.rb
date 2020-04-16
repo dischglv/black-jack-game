7c4827ff77ea96bc53269a4e76bfdcebe3c92885
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-16 10:28:21 +0300

    Adding initial score constant

--- player.rb
+++ player.rb
@@ -1,11 +1,12 @@
 class Player
   include Bank
+  INITIAL_SCORE = 100
 
   attr_reader :name
   attr_accessor :cards
 
   def initialize(name)
-    @score = 100
+    @score = INITIAL_SCORE
     @name = name
     @cards = []
   end

9b65c135f24dfd6fe23505926d5def91d031f614
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-16 08:57:55 +0300

    Implementing Bank module

--- bank.rb
+++ bank.rb
@@ -1,2 +1,15 @@
 module Bank
+  attr_accessor :score
+
+  def take(amount)
+    if score > amount
+      self.score -= amount
+    else
+      raise ArgumentError, 'Argument is greater than score'
+    end
+  end
+
+  def put(amount)
+    self.score += amount
+  end
 end

d1c54f9d6f56e2ca879e6d3b2da340cfda702a1c
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-17 09:21:18 +0300

    Implementing show_game_status

--- game.rb
+++ game.rb
@@ -12,30 +12,60 @@ class Game
 
   def start
     name = ui.request('Введите имя пользователя')
-    user = UserPlayer.new(name)
-    dealer = ComputerPlayer.new(DEALER_NAME)
+    user = UserPlayer.new(name, self)
+    dealer = ComputerPlayer.new(DEALER_NAME, self)
     players << user << dealer
     play_round
   end
 
   private
-  attr_accessor :players, :cards_opened, :game_over
+  attr_accessor :players, :cards_opened
 
   def play_round
     loop do
       self.cards_opened = false
-      self.game_over = false
       players.each do |player|
         player.cards = Card.random_deck(INITIAL_DECK_SIZE)
         player.give_to self, BID_AMOUNT
       end
+      show_game_status
 
-      until game_over?
+      until cards_opened?
+        ui.puts("Ход игрока #{players.first.name}...")
         players.first.perform_turn
         players.rotate!
+        self.cards_opened = true if maximum_deck_size?
       end
+      # подсчет результатов, вывод, перевод денег из банка игры
 
       break unless ui.ask('Хотите сыграть еще раз?')
     end
+  end
+
+  def show_game_status
+    ui.puts("----------------------------------")
+    ui.puts("Банк игры: #{score}$")
+    players.each { |player| ui.puts("Банк #{player.name}: #{player.score}$") }
+    players.each do |player|
+      if player.is_a? UserPlayer or player.is_a? ComputerPlayer && cards_opened?
+        ui.print("#{player.name}:  ")
+        player.cards.each { |card| ui.print("#{card} ") }
+        ui.puts(",  всего очков: #{count_points(player)}")
+      elsif player.is_a? ComputerPlayer && !cards_opened?
+        ui.print("#{player.name}:  ")
+        player.cards.each { |card| ui.print("\u1f0a0 ") }
+        ui.puts("")
+      end
+    end
+    ui.puts("----------------------------------")
+  end
+
+  def cards_opened?
+  end
+
+  def maximum_deck_size?
+  end
+
+  def count_points(player)
   end
 end

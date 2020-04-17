b7bc772fdc89a6af9a385af81fdd0329cf2ace61
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-17 12:29:49 +0300

    Implementing count_points

--- game.rb
+++ game.rb
@@ -43,6 +43,7 @@ class Game
     end
   end
 
+  # refacorme
   def show_game_status
     ui.puts("----------------------------------")
     ui.puts("Банк игры: #{score}$")
@@ -72,5 +73,28 @@ class Game
   end
 
   def count_points(player)
+    points = 0
+    cards = player.cards
+    points_counter = Proc.new do |card|
+      case card.rank
+      when 'K', 'Q', 'J'
+        points += 10
+      when 'A'
+        points += 1
+      else
+        points += card.rank.to_i
+      end
+    end
+
+    if cards.any? { |card| card.rank == 'A'}
+      cards.sort! do |first, second|
+        first.rank == 'A' ? 1 : 0
+      end
+      cards[0...-1].each &points_counter
+      points += points + 11 > 21 ? 1 : 11
+    else
+      cards.each &points_counter
+    end
+    points
   end
 end

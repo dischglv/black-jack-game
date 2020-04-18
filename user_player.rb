b9842f00cce3797f2f91d3446571489a5e3d6468
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-18 16:17:36 +0300

    Implementing perform_turn for user player

--- user_player.rb
+++ user_player.rb
@@ -1,2 +1,13 @@
 class UserPlayer < Player
+  def perform_turn
+    turn = ui.hash_choose('ход', turns)
+    self.send turn
+  rescue RuntimeError => e
+    if e.message == 'Cannot add more cards, deck is a maximum size'
+      ui.puts('Больше добавить карт нельзя, выберите другой ход')
+      retry
+    else
+      raise e
+    end
+  end
 end

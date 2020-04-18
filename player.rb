5e7a48f71025a4cf341f0fff3beaae9748c77d28
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-18 15:01:44 +0300

    Adding turns to player class

--- player.rb
+++ player.rb
@@ -18,4 +18,17 @@ class Player
 
   protected
   attr_accessor :game
+
+  def skip_turn
+  end
+
+  def add_card
+    raise 'Cannot add more cards, deck is a maximum size' if deck_size == game.maximum_deck_size
+    cards << Card.random
+    game.show_game_status
+  end
+
+  def open_cards
+    game.cards_open = true
+  end
 end

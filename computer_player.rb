4bf7b0b5229be32fcdd06d8886bbf3c5bb6b68ab
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-18 19:56:24 +0300

    Fixing perform turn function

--- computer_player.rb
+++ computer_player.rb
@@ -1,6 +1,6 @@
 class ComputerPlayer < Player
   def perform_turn
-    if score < 17 && deck_size < game.maximum_deck_size
+    if game.count_points(self) < 17 && deck_size < game.maximum_deck_size
       add_card
     else
       skip_turn

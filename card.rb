897ae363e35a5c008890154b407f7ab991d6594c
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-19 22:29:53 +0300

    Fixing card class

--- card.rb
+++ card.rb
@@ -1,20 +1,6 @@
 class Card
   attr_reader :suit, :rank
 
-  class << self
-    def random
-      suit = all_suits.keys.sample
-      rank = all_ranks.sample
-      new(suit, rank)
-    end
-
-    def random_deck(deck_size)
-      deck = []
-      deck_size.times { deck << random }
-      deck
-    end
-  end
-
   def initialize(suit, rank)
     @suit = suit
     @rank = rank
@@ -23,8 +9,6 @@ class Card
   def to_s
     rank + self.class.all_suits[suit]
   end
-
-  private
 
   class << self
     def all_suits

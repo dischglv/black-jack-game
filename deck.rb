e806f5bea9c11a03e6599d5f09e43a5a84e284cc
Diana Shchegoleva <dschegoleva@gmail.com>
2020-04-19 22:29:24 +0300

    Fixing deck class

--- deck.rb
+++ deck.rb
@@ -1,20 +1,18 @@
 class Deck
   attr_reader :cards
 
-  class << self
-    def full_deck
-      result = []
-      Card.all_suits.each_key do |suit|
-        Card.all_ranks.each do |rank|
-          result << Card.new(suit, rank)
-        end
-      end
-      result
-    end
-  end
-
   def initialize
-    @cards = self.class.full_deck
+    @cards = full_deck
+  end
+  
+  def full_deck
+    result = []
+    Card.all_suits.each_key do |suit|
+      Card.all_ranks.each do |rank|
+        result << Card.new(suit, rank)
+      end
+    end
+    result
   end
 
   def give_cards(number = 1)
